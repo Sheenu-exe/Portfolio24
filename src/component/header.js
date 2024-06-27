5331a4657106ea41f64c6bfc1f0bc3ab10bc71ec
Sachin Parihar <pariharsachin5002@gmail.com>
2024-06-27 18:56:44 +0530

    resume pdf

--- src/component/header.js
+++ src/component/header.js
@@ -12,14 +12,14 @@ export const Header = () => {
        
       </div>
       <ul className="sm:flex hidden px-1.5 py-1.5 gap-1.5 bg-black/50 rounded-md">
-          <li className="px-2 py-1 rounded-md transition-all hover:ease-in-out cursor-pointer text-white hover:bg-white hover:text-black  active:bg-slate-100/50  active:text-black focus:text-white  focus:bg-slate-100"><a href="#home">Home</a></li>
-          <li className="px-2 py-1 rounded-md transition-all hover:ease-in-out cursor-pointer text-white hover:bg-white hover:text-black active:bg-slate-100/50 active:text-black focus:text-white  focus:bg-slate-100"><a href="#techstack">Tech Stack</a></li>
-          <li className="px-2 py-1 rounded-md transition-all hover:ease-in-out cursor-pointer text-white hover:bg-white hover:text-black active:bg-slate-100/50 active:text-black focus:text-white focus:bg-slate-100"><a href="#projects">Projects</a></li>
-          <li className="px-2 py-1 rounded-md transition-all hover:ease-in-out cursor-pointer text-white hover:bg-white hover:text-black active:bg-slate-100/50 active:text-black focus:text-white focus:bg-slate-100"><a href="#about">About</a></li>
+          <li className="px-2 py-1 rounded-lg transition-all hover:ease-in-out cursor-pointer text-white hover:bg-white hover:text-black  active:bg-slate-100/50  active:text-black focus:text-white  focus:bg-slate-100"><a href="#home">Home</a></li>
+          <li className="px-2 py-1 rounded-lg transition-all hover:ease-in-out cursor-pointer text-white hover:bg-white hover:text-black active:bg-slate-100/50 active:text-black focus:text-white  focus:bg-slate-100"><a href="#techstack">Tech Stack</a></li>
+          <li className="px-2 py-1 rounded-lg transition-all hover:ease-in-out cursor-pointer text-white hover:bg-white hover:text-black active:bg-slate-100/50 active:text-black focus:text-white focus:bg-slate-100"><a href="#projects">Projects</a></li>
+          <li className="px-2 py-1 rounded-lg transition-all hover:ease-in-out cursor-pointer text-white hover:bg-white hover:text-black active:bg-slate-100/50 active:text-black focus:text-white focus:bg-slate-100"><a href="#about">About</a></li>
         </ul>
       
-      <div className="flex flex-col bg-zinc-900 mr-2 h-[90%] w-auto px-2 py-1 rounded-lg tracking-tight justify-center items-center">
-        <a href="https://drive.google.com/file/d/1FN19ClpiZgEp0X729lsrLv9X6Ih1Ry-s/view?usp=drive_link"><PiFilePdfFill className="text-white text-3xl"/></a>
+      <div className="flex flex-col bg-zinc-900 hover:bg-white transition-all mr-2 h-[90%] w-auto px-2 py-1 rounded-lg tracking-tight justify-center items-center">
+        <a href="https://drive.google.com/file/d/1FN19ClpiZgEp0X729lsrLv9X6Ih1Ry-s/view?usp=drive_link"><PiFilePdfFill className="text-white hover:text-black text-3xl"/></a>
       </div>
     </div>
   );
